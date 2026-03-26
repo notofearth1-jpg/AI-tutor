b56d77a2a51314b0384b17f5778e71a25856dac0
Vibhu <vibhu.thankii@gmail.com>
2026-03-26 18:14:41 +0530

    fix: standardize backend on CommonJS to resolve TS conflicts

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -1,10 +1,6 @@
 import { z } from "zod";
 import * as dotenv from "dotenv";
 import * as path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // Load from root
 dotenv.config({ path: path.join(__dirname, "../../../.env") });
